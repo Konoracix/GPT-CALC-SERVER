965afcd6b512e96e5995cdbc1c1f5cb3416e661f
Radosław <radek.barylak@gmail.com>
2025-08-30 17:17:46 +0200

    Fix

--- GPT_CALC_SERVER/GPT_CALC_SERVER/settings.py
+++ GPT_CALC_SERVER/GPT_CALC_SERVER/settings.py
@@ -131,9 +131,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Middleware Whitenoise do serwowania statycznych plików w prod
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <- to jest ważne
+    # ... reszta middleware
+]
+
+# Opcjonalnie kompresja statycznych plików
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
