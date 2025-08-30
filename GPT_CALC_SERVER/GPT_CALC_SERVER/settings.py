ee5200a54e268ed6eca16b832a1f50e577c58204
Radosław <radek.barylak@gmail.com>
2025-08-30 17:20:56 +0200

    Fix

--- GPT_CALC_SERVER/GPT_CALC_SERVER/settings.py
+++ GPT_CALC_SERVER/GPT_CALC_SERVER/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <- to jest ważne
 ]
 
 ROOT_URLCONF = 'GPT_CALC_SERVER.urls'
@@ -135,18 +136,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-# Middleware Whitenoise do serwowania statycznych plików w prod
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # <- to jest ważne
-    # ... reszta middleware
-]
-
-# Opcjonalnie kompresja statycznych plików
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
